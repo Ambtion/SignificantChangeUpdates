b7506f481071c89ac76b9ac8636312ecd382fa8d
LingJunHou <kequ1988@gmail.com>
2016-02-26 15:52:42 +0800

    Initial Commit

---
+++ SignificantChangeUpdates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SignificantChangeUpdates
+//
+//  Created by quke on 16/2/26.
+//  Copyright © 2016年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SignificantChangeUpdates/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SignificantChangeUpdates
+//
+//  Created by quke on 16/2/26.
+//  Copyright © 2016年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
